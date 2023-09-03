5d35f97eb266341d31993224f07be5055169991a
Aleksandr Borisov <aleksandr-zm-borisov@mail.ru>
2023-09-03 23:10:15 +0800

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.AlreadyExists.LikeAlreadyExistsException;
 import ru.yandex.practicum.filmorate.exception.NotFound.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.exception.NotFound.LikeNotFoundException;
 import ru.yandex.practicum.filmorate.exception.NotFound.UserNotFoundException;
